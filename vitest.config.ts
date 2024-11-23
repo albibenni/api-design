bf2d3e0cd5c695afcced1bf8c7cdd011ae4374a7
Alberto Benatti <albi.benni8@gmail.com>
2024-11-23 10:51:54 +0100

    init

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import { config } from "dotenv";
+
+export default defineConfig({
+  test: {
+    // globalSetup: "./src/__tests__/global.setup.ts",
+    passWithNoTests: true,
+    globals: true,
+    fileParallelism: false,
+    poolOptions: {
+      maxWorkers: 1,
+    },
+    env: {
+      ...config({ path: "./.env.local" }).parsed,
+    },
+    root: "./packages",
+    onConsoleLog(log: string, type: "stdout" | "stderr"): false | void {
+      console.log("log in test: ", log);
+      if (log === "message from third party library" && type === "stdout") {
+        return false;
+      }
+    },
+  },
+});
