87a9edd180a775108783ae18c431f447877f8247
Alberto Benatti <albi.benni8@gmail.com>
2024-11-24 18:43:15 +0100

    auth

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,15 +1,39 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { User } from "@prisma/client";
 
-export const comparePasswords = (password: string, hash: string) => {
+/**
+ * Compares a plaintext password with a hashed password.
+ *
+ * @param password - The plaintext password to compare.
+ * @param hash - The hashed password to compare against.
+ * @returns A promise that resolves to a boolean indicating whether the passwords match.
+ */
+export const comparePasswords = (
+  password: string,
+  hash: string,
+): Promise<boolean> => {
   return bcrypt.compare(password, hash);
 };
 
-export const hashPassword = (password: string) => {
+/**
+ * Hashes a given password using bcrypt with a salt rounds value of 5.
+ *
+ * @param password - The plain text password to be hashed.
+ * @returns A promise that resolves to the hashed password string.
+ */
+export const hashPassword = (password: string): Promise<string> => {
   return bcrypt.hash(password, 5);
 };
 
-export const createJWT = (user: any) => {
+/**
+ * Generates a JSON Web Token (JWT) for the given user.
+ *
+ * @param {User} user - The user object containing user details.
+ * @returns {string} - The generated JWT token.
+ * @throws {Error} - Throws an error if the JWT secret is not found in the environment variables.
+ */
+export const createJWT = (user: User): string => {
   const jwt_secret = process.env.JWT_SECRET;
   if (!jwt_secret) {
     throw new Error("No JWT secret found");
@@ -24,7 +48,20 @@ export const createJWT = (user: any) => {
   return token;
 };
 
-export const protect = (req: any, res: any, next: any) => {
+/**
+ * Middleware to protect routes by verifying the JWT token.
+ *
+ * This middleware checks for the presence of a JWT token in the `Authorization` header of the request.
+ * If the token is valid, it attaches the decoded user information to the `req` object and calls the `next` middleware.
+ * If the token is missing or invalid, it responds with a 401 status and an appropriate error message.
+ *
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - The next middleware function.
+ *
+ * @throws Will throw an error if the JWT secret is not found in the environment variables.
+ */
+export const protect = (req: any, res: any, next: any): void => {
   const jwt_secret = process.env.JWT_SECRET;
   if (!jwt_secret) {
     throw new Error("No JWT secret found");
