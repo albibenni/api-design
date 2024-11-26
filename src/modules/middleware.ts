e6834e771e314e05fc4b552422029bf899ac9755
Alberto Benatti <albi.benni8@gmail.com>
2024-11-26 12:18:46 +0100

    test middleware logic

--- src/modules/middleware.ts
+++ src/modules/middleware.ts
@@ -2,11 +2,11 @@ import { validationResult } from "express-validator";
 
 /**
  * Middleware function to handle input validation errors.
- * 
+ *
  * This function checks for validation errors in the request object.
  * If there are errors, it responds with a 400 status code and a JSON object containing the errors.
  * If there are no errors, it calls the next middleware function.
- * 
+ *
  * @param req - The request object.
  * @param res - The response object.
  * @param next - The next middleware function.
