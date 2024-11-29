7467b3c9e2a9245dac46cc696e263c471c32c0c0
Alberto Benatti <albi.benni8@gmail.com>
2024-11-29 10:17:09 +0100

    error for user and doc

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,19 +1,40 @@
 import { User } from "@prisma/client";
 import prisma from "../db.js";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth.js";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-export const createNewUser = async (req: Request, res: Response) => {
-  const user: User = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: await hashPassword(req.body.password),
-    },
-  });
-  await prisma.$disconnect(); // check if this is necessary
+/**
+ * Creates a new user in the database.
+ *
+ * @param req - The request object containing the user data.
+ * @param res - The response object to send the response.
+ * @param next - The next middleware function in the stack.
+ *
+ * @returns A JSON response containing a JWT token for the newly created user.
+ *
+ * @throws Will throw an error if user creation fails.
+ */
+export const createNewUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const user: User = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: await hashPassword(req.body.password),
+      },
+    });
+    await prisma.$disconnect(); // check if this is necessary
 
-  const token = createJWT(user);
-  res.json({ token });
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error: any) {
+    console.error(error.message);
+    error.type = "input";
+    next(error);
+  }
 };
 
 export const signin = async (req: Request, res: Response) => {
